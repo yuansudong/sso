1c77dde6e5266001b40e883af8212a62f8ecd7e5
yuansudong <yuansudong@phoenix-ea.com>
2021-06-02 16:32:09 +0800

    更新

--- main.go
+++ main.go
@@ -1,5 +1,4 @@
-The MIT License (MIT)
-
+/*
 Copyright © 2021 yuansudong
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -19,3 +18,42 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
+*/
+package main
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+// GetClientCommand
+func GetClientCommnd() *cobra.Command {
+
+	mRootCommand := cobra.Command{
+		Use:   "client",
+		Long:  "client-long",
+		Short: "client-short",
+	}
+	mUpdateGD := &cobra.Command{
+		Use:   "update_gd",
+		Long:  "update_gd_long",
+		Short: "update_gd_short",
+		Run: func(cmd *cobra.Command, args []string) {
+			log.Println(cmd.PersistentFlags().GetString("listen_addr"))
+			log.Println("call client update_gd", args, cmd.Example)
+		},
+	}
+	mUpdateGD.Flags().String("listen_addr", "127.0.0.1", "--listen_addr=:8080")
+	mRootCommand.AddCommand(mUpdateGD)
+
+	return &mRootCommand
+}
+
+func main() {
+	mRootCmd := cobra.Command{Use: "docker", Long: "虚拟化引擎-long", Short: "虚拟化引擎-short"}
+	mRootCmd.AddCommand(GetClientCommnd())
+	if err := mRootCmd.Execute(); err != nil {
+		log.Println(err.Error())
+	}
+}
